04f5f5345d91afb4a50641fea44ff723265326d5
EraldoOL <eo80797@gmail.com>
2025-01-16 17:04:41 -0300

    update index.js

--- personal-trainer-platform/backend/index.js
+++ personal-trainer-platform/backend/index.js
@@ -3,19 +3,29 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+const studentRoutes = require('./routes/studentRoutes');
+const notificationRoutes = require('./routes/notificationRoutes');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conectado ao MongoDB'))
-    .catch(err => console.error(err));
+  .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+  .catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+    process.exit(1);
+  });
 
 // Rotas
-app.get('/', (req, res) => res.send('API Funcionando'));
+app.use('/api/students', studentRoutes);
+app.use('/api/notifications', notificationRoutes);
 
+app.get('/', (req, res) => res.send('API funcionando!'));
+
+// Inicia o servidor
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
